a7a118dcdbd6b1585a7c447be8431e48a7f9189b
spaceape3001 <spaceape3001@users.noreply.github.com>
2022-08-11 08:58:12 +0000

    Nixing from here
    Moving these to yq-vulqan

--- stb.cmake
+++
@@ -1,8 +0,0 @@
-if(NOT TARGET stb)
-    set(stb_dir ${CMAKE_CURRENT_LIST_DIR}/stb)
-    add_library(stb SHARED
-        ${CMAKE_CURRENT_LIST_DIR}/stb.cpp
-    )
-    target_include_directories(stb PUBLIC ${stb_dir})
-endif()
-
