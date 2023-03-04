0fafea0c26c3a2f3f26c8af9cdabac3b1eb28585
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-03-04 12:36:24 +0000

    Initial Skeleton

--- configure.cmake
+++ configure.cmake
@@ -1,4 +1,4 @@
-if(NOT YOUR_QUILL_EXTERNAL)
+if(NOT YOUR_QUILL_QTUTIL)
 
     if( "${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}")
             # Mixing source and binary output is asking for trouble, best to avoid it
@@ -21,7 +21,7 @@ if(NOT YOUR_QUILL_EXTERNAL)
     endif()
 
     set(CMAKE_CONFIGURATION_TYPES Debug;Release)
-    set(CMAKE_CXX_STANDARD 23)                  # C++20 is the standard (may be bumped in the future)
+    set(CMAKE_CXX_STANDARD 23)                  # C++23 is the standard (may be bumped in the future)
     set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
     set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY  ${CMAKE_BINARY_DIR}/bin)
@@ -30,6 +30,6 @@ if(NOT YOUR_QUILL_EXTERNAL)
     set(CMAKE_TEST_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
     set(CMAKE_DEBUG_POSTFIX D)
 
-    set(YOUR_QUILL_EXTERNAL ${CMAKE_CURRENT_LIST_DIR})
+    set(YOUR_QUILL_QTUTIL ${CMAKE_CURRENT_LIST_DIR})
 endif()
 
