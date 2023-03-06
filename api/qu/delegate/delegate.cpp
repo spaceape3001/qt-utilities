8c7d85fb76f9013f25284ace8a78b51f66e24bfa
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-03-06 00:13:03 +0000

    Delegates
    Adding Delegates (& Color ones)

---
+++ api/qu/delegate/delegate.cpp
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL's QT UTILITIES
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "ColorComboBoxDelegate.ipp"
+#include "ColorDelegate.ipp"
+#include "Delegate.ipp"
+
+#include "moc_ColorComboBoxDelegate.cpp"
+#include "moc_ColorDelegate.cpp"
+#include "moc_Delegate.cpp"
