f7a153b59059bf2f652a2f42846d014eda73a7ca
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-07-31 07:35:22 +0000

    Doxygen

--- api/gluon/app/Application.hpp
+++ api/gluon/app/Application.hpp
@@ -10,12 +10,22 @@
 #include <QApplication>
 
 namespace yq::gluon {
+
+    /*! \brief Application class for the library
+    
+        This application class binds the Qt with the toolbox
+    */
     class Application : public QApplication, public BasicApp {
         Q_OBJECT
     public:
+    
+        //! Current instance (may be NULL)
         static Application*     app();
     
+        //! Constructor
         Application(int&, char**);
+        
+        //! Destructor
         ~Application();
         
     private:
