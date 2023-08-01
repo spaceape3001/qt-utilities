ce874c24cf5dad854bb5722389caa02b2aef9339
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-01 11:02:19 +0000

    Doxygen

--- api/gluon/app/SimpleEditor.hpp
+++ api/gluon/app/SimpleEditor.hpp
@@ -13,32 +13,61 @@ class QUndoStack;
 
 namespace yq::gluon {
 
+    /*! \brief Simple Editor
+    
+        This implements a simple main window as an edtor.
+    */
     class SimpleEditor : public UndoMainWindow
     {
         Q_OBJECT
     public:
 
+        //! Override to catch the "okay-to-close" 
         bool    okayToClose() override;
         
+        //! Our filename, broken into bits
         const FileName&   file() const { return m_file; }
 
     public slots:
+        //! New file
         void            cmdNew();
+        
+        //! Open file
         void            cmdOpen();
+        
+        //! Save the file
         void            cmdSave();
+        
+        //! Save the file to a new filename
         void            cmdSaveAs();
+        
+        //! Update the title bar
         virtual void    updateTitle();
         
     protected:
+        //! Constructor, takes in an editor widget
         SimpleEditor(QWidget* editor);
+        
+        //! Destructor
         ~SimpleEditor();
         
+        //! Our data type name
         virtual QString     ourData() const { return tr("Data"); }
+        
+        //! Our app name
         virtual QString     ourName() const { return tr("SimpleEditor"); }
+        
+        //! Our save type
         virtual QString     ourSaveType() const { return QString(); }
         
+        
+        //! New file (please override)
         virtual bool        newFile() { return false; }
+        
+        //! Open file (please override)
         virtual bool        openFile(const QString&) { return false; }
+        
+        //! Save file (please override)
         virtual bool        saveFile(const QString&) const { return false; }
         
         //  Override to alter open-file-dialog
