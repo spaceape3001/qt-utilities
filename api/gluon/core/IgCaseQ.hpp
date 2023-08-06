d3aa6e68d6cd1ecaa97bd61bb5d4ffe489e34509
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-06 04:51:51 +0000

    Doxygen

--- api/gluon/core/IgCaseQ.hpp
+++ api/gluon/core/IgCaseQ.hpp
@@ -12,9 +12,20 @@ class QString;
 class QByteArray;
 
 namespace yq::gluon {
+
+    //! String Comparison to ignore case
+    //!
+    //! Standard empty class that's comparing strings & making them 
+    //! Case-insensitive
     struct IgCaseQ : public IgCase {
+    
+        //! Allow for the normal comparisons
         using IgCase::operator();
+        
+        //! QString comparison
         bool  operator()(const QString&, const QString&) const;
+
+        //! QByteArray comparison
         bool  operator()(const QByteArray&, const QByteArray&) const;
     };
     
