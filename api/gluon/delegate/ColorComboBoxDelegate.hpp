eab78f9afc161408322d8fae4c3c8a4d32a70362
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-08 05:02:08 +0000

    Doxygen

--- api/gluon/delegate/ColorComboBoxDelegate.hpp
+++ api/gluon/delegate/ColorComboBoxDelegate.hpp
@@ -10,6 +10,8 @@
 
 namespace yq::gluon {
 
+    /*! \brief Delegate for QItemModel that uses the ColorComboBox
+    */
     class  ColorComboBoxDelegate : public Delegate {
         Q_OBJECT
         YQ_OBJECT_DECLARE(ColorComboBoxDelegate, Delegate)
@@ -17,13 +19,25 @@ namespace yq::gluon {
 
         using DataType = QColor;
 
+        //! Creates the editor
         virtual QWidget*    createEditor(QWidget* parent=nullptr) const;
+
+        //! Sets the editor data
         virtual bool        setEditorData(QWidget*, const QVariant&) const;
+
+        //! Gets the editor data
         virtual QVariant    getEditorData(const QWidget*) const;
+
+        //! Paints the cell
         virtual bool        paint(QPainter*, const QStyleOptionViewItem& option, const QVariant&) const;
+
+        //! Connect the "edited" signal
         virtual void        connectEditedSignal(QWidget*, QObject*, const char*) const;
 
+        //! Constructor
         ColorComboBoxDelegate(QObject* parent=nullptr);
+        
+        //! Destructor
         virtual ~ColorComboBoxDelegate();
     };
 }
