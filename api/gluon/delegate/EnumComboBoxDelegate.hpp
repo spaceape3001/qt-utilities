6b6f673953352e39ca45d8a3f81e44b88f9506f5
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-10 16:10:42 +0000

    Doxygen

--- api/gluon/delegate/EnumComboBoxDelegate.hpp
+++ api/gluon/delegate/EnumComboBoxDelegate.hpp
@@ -11,7 +11,7 @@
 #include <QMetaType>
 
 namespace yq::gluon {
-    /*! \brief Enumeration Delegate
+    /*! \brief Generic Enumeration Delegate
 
         The render/create can be any of integer, string, and/or the enumeration.
         The setData will be in the enum type.
@@ -20,15 +20,28 @@ namespace yq::gluon {
         Q_OBJECT
         YQ_OBJECT_DECLARE(GenericEnumComboBoxDelegate, Delegate)
     public:
+    
+        //! Constructor
         GenericEnumComboBoxDelegate(const EnumDef*, QMetaType type, QObject *parent=nullptr);
         ~GenericEnumComboBoxDelegate();
         
+        //! Renders the data
         virtual QVariant    render(const QVariant&) const override;
+        
+        //! Creates an editor
         virtual QWidget*    createEditor(QWidget* parent=nullptr) const;
+        
+        //! Sets editor data
         virtual bool        setEditorData(QWidget*, const QVariant&) const;
+        
+        //! Gets editor data
         virtual QVariant    getEditorData(const QWidget*) const;
         //virtual bool        paint(QPainter*, const QStyleOptionViewItem& option, const QVariant&) const;
+        
+        //! Connects the edited signal
         virtual void        connectEditedSignal(QWidget*, QObject*, const char*) const;
+        
+        //! Display text for value
         virtual QString 	displayText(const QVariant &value, const QLocale &locale) const override;    
 
     private:
@@ -36,6 +49,11 @@ namespace yq::gluon {
         QMetaType           m_type;
     };
 
+
+    /*! \brief Specific enum combo box delegate
+    
+        This is the derivation to Generic, binding to a specific enumeration type by meta info
+    */
     template <typename E>
     class EnumComboBoxDelegate : public GenericEnumComboBoxDelegate {
     public:
