ba330bf7ecd44c868f3570ceb4343741805b3185
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-11 08:43:34 +0000

    Doxygen

--- api/gluon/delegate/FileBoxDelegate.hpp
+++ api/gluon/delegate/FileBoxDelegate.hpp
@@ -9,18 +9,42 @@
 #include <gluon/delegate/Delegate.hpp>
 
 namespace yq::gluon {
+
+    /*! \brief File Box Delegate for browsing files
+    
+        This makes a file-box as a delegate for browsing files
+    */
     class FileBoxDelegate : public Delegate {
         Q_OBJECT
         YQ_OBJECT_DECLARE(FileBoxDelegate, Delegate)
     public:
+    
+        //! Constructor
         FileBoxDelegate(QObject*parent =nullptr);
+        
+        //! Destructor
         ~FileBoxDelegate();
-        virtual yq::Compare compare(const QVariant&, const QVariant&) const override; 
+        
+        //! Compare two values
+        virtual Compare     compare(const QVariant&, const QVariant&) const override; 
+        
+        //! Create editor
         virtual QWidget*    createEditor(QWidget* parent=nullptr) const;
+        
+        //! Set editor data
         virtual bool        setEditorData(QWidget*, const QVariant&) const;
+        
+        //! Get editor data
         virtual QVariant    getEditorData(const QWidget*) const;
         //virtual bool        paint(QPainter*, const QStyleOptionViewItem& option, const QVariant&) const;
+        
+        //! Connect edited signal
         virtual void        connectEditedSignal(QWidget*, QObject*, const char*) const;
+        
+        
+        //  TODO: File Name Filter (propagated into file box itself)
+        //  TODO: File Dialog Title
+        //  TODO: Consistent Directory location
     };
 }
 
