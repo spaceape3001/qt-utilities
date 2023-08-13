88f05ab78665c27c957a0b5fe58de3fcd2f2ad04
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-13 06:07:34 +0000

    Doxygen

--- api/gluon/delegate/ImageDelegate.hpp
+++ api/gluon/delegate/ImageDelegate.hpp
@@ -12,17 +12,27 @@ class QImage;
 
 namespace yq::gluon {
 
+    /*! \brief Image Delegate
+    
+        Use this when you expect the data to be an image
+    */
     class ImageDelegate : public Delegate {
         Q_OBJECT
         YQ_OBJECT_DECLARE(ImageDelegate, Delegate)
     public:
         using DataType = QImage;
+        
+        //! Constructor
         ImageDelegate(QObject* parent=nullptr);
+        
+        //! Destructor
         virtual ~ImageDelegate();
         
+        //! Paints the cell
         virtual bool        paint(QPainter*, const QStyleOptionViewItem& option, const QVariant&) const override;
-        virtual QSize       sizeHint(const QStyleOptionViewItem &option,
-                                const QModelIndex &index) const override;
+        
+        //! Size hint for the cell
+        virtual QSize       sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
     };
 
 }
