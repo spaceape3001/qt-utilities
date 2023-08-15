c4d41618243f43f965fe761f976f3c54fbe5d927
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-15 06:25:53 +0000

    Doxygen

--- api/gluon/delegate/NullableDateDelegate.hpp
+++ api/gluon/delegate/NullableDateDelegate.hpp
@@ -10,6 +10,10 @@
 
 namespace yq::gluon {
 
+    /*! \brief Date Delegate
+    
+        This date delegate allows for null dates, assumes the data is QDate
+    */
     class NullableDateDelegate : public Delegate {
         Q_OBJECT
         YQ_OBJECT_DECLARE(NullableDateDelegate, Delegate)
@@ -17,14 +21,28 @@ namespace yq::gluon {
 
         using DataType  = QDate;
 
+        //! Constructor
         NullableDateDelegate(QObject*parent=nullptr);
-        virtual ~NullableDateDelegate();
         
+        //! Destructor
+        virtual ~NullableDateDelegate();
+
+        //! Renders the variant to something that can be displayed (ie text)
         virtual QVariant    render(const QVariant&) const override;
-        virtual yq::Compare compare(const QVariant&, const QVariant&) const override; 
+        
+        //! Compares two values
+        virtual Compare     compare(const QVariant&, const QVariant&) const override; 
+        
+        //! Creates an editor
         virtual QWidget*    createEditor(QWidget* parent=nullptr) const override;
+        
+        //! Sets the editor data
         virtual bool        setEditorData(QWidget*, const QVariant&) const override;
+        
+        //! Gets the editor data
         virtual QVariant    getEditorData(const QWidget*) const override;
+        
+        //! Connects the "edited" signal
         virtual void        connectEditedSignal(QWidget*, QObject*, const char*) const override;
     };
 }
