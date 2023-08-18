d33c2a945f0aff8b2b90172e4f52c155fd100181
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-18 08:40:18 +0000

    Doxygen
    Yeah, the filler commit, a bit of doxygen so I can get my daily
    commit/push in.

--- api/gluon/delegate/StringDelegate.hpp
+++ api/gluon/delegate/StringDelegate.hpp
@@ -9,6 +9,11 @@
 #include <gluon/delegate/Delegate.hpp>
 
 namespace yq::gluon {
+
+    /*! \brief String Delegate
+    
+        This is a string-edit delegate using a line-edit with a validator
+    */
     class StringDelegate : public Delegate {
         Q_OBJECT
         YQ_OBJECT_DECLARE(StringDelegate, Delegate)
@@ -16,14 +21,28 @@ namespace yq::gluon {
         using DataType      = QString;
 
         //virtual QVariant    render(const QVariant&) const override;
-        virtual yq::Compare compare(const QVariant&, const QVariant&) const override; 
+        //! Compares the two items (ignores the case)
+        virtual Compare     compare(const QVariant&, const QVariant&) const override; 
+        
+        //! Creates the editor
         virtual QWidget*    createEditor(QWidget* parent=nullptr) const override;
+        
+        //! Sets the editor's data
         virtual bool        setEditorData(QWidget*, const QVariant&) const override;
+        
+        //! Gets the editor's data
         virtual QVariant    getEditorData(const QWidget*) const override;
+        
+        //! Connects the edited signal
         virtual void        connectEditedSignal(QWidget*, QObject*, const char*) const override;
 
+        //! Standard constructor
         StringDelegate(QObject*parent=nullptr);
+        
+        //! Constructor with a validator
         StringDelegate(QValidator*, QObject*parent=nullptr);
+        
+        //! Destructor
         virtual ~StringDelegate();
     private:
         QValidator*         m_validator;
