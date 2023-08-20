c8e9680752c411640608ed8af75fb3675bcca965
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-20 05:51:00 +0000

    Doxygen

--- api/gluon/edit/CheckBox.hpp
+++ api/gluon/edit/CheckBox.hpp
@@ -18,13 +18,22 @@ namespace yq::gluon {
     class CheckBox : public QCheckBox, public UndoBase {
         Q_OBJECT
     public:
+        
+        //! Constructor
         CheckBox(QWidget* parent=nullptr);
+        
+        //! Constructor with label
         CheckBox(const QString& text, QWidget* parent=nullptr);
+        
+        //! Destruct
         virtual ~CheckBox();
         
         //  TODO: Currently, the *ACTUAL* undo tie in is not yet implemented
 
+        //! True if we're checked
         bool    isChecked() const;
+        
+        //! Sets the checked state (on/off)
         void    setChecked(bool);
     };
 
