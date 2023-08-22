0cf89a53b93f3e9699c5d8d8557bd6f0017ee8c4
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-22 08:53:56 +0000

    Doxygen

--- api/gluon/edit/ColorComboBox.hpp
+++ api/gluon/edit/ColorComboBox.hpp
@@ -12,14 +12,22 @@ class QColor;
 
 namespace yq::gluon {
 
+    //! Combo box of colors
     class ColorComboBox : public ComboBox {
         Q_OBJECT
         Q_PROPERTY(QColor color READ color WRITE setColor USER true)
     public:
+    
+        //! Constructor
         ColorComboBox(QWidget* parent=nullptr);
+        
+        //! Destructor
         virtual ~ColorComboBox();
         
+        //! Gets the color
         QColor color() const;
+        
+        //! Sets the color
         void    setColor(QColor);
         
         //  TODO: Currently, the *ACTUAL* undo tie in is not yet implemented
