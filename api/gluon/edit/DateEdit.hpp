0f0b265fb9bc39322c02af308da8b92b80a404e7
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-24 10:53:45 +0000

    Doxygen
    Forgot to push earlier this morning, doing it now.

--- api/gluon/edit/DateEdit.hpp
+++ api/gluon/edit/DateEdit.hpp
@@ -18,7 +18,11 @@ namespace yq::gluon {
     class DateEdit : public QDateEdit, public UndoBase {
         Q_OBJECT
     public:
+    
+        //! Constructor
         DateEdit(QWidget* parent = nullptr);
+        
+        //! Destructor
         ~DateEdit();
         //  TODO: Currently, the *ACTUAL* undo tie in is not yet implemented
     };
