91b2f27e9e8711a1afee9a43718c53d391a7230c
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-08-27 13:51:10 +0000

    Doxygen

--- api/gluon/edit/DoubleSpinBox.hpp
+++ api/gluon/edit/DoubleSpinBox.hpp
@@ -10,15 +10,25 @@
 #include <gluon/undo/UndoBase.hpp>
 
 namespace yq::gluon {
+
+    /*! \brief Double Spin Box with Undo Capability
+    
+        (well, eventually, not right now, the undo is actually TODO)
+    */
     class DoubleSpinBox : public QDoubleSpinBox, public UndoBase {
         Q_OBJECT
     public:
+    
+        //! Constructor
         DoubleSpinBox(QWidget*parent=nullptr);
+        
+        //! Destructor
         ~DoubleSpinBox();
         
         //  TODO: Currently, the *ACTUAL* undo tie in is not yet implemented
 
     public slots:   
+        //! Reset to default
         virtual void    reset();
     };
 }
