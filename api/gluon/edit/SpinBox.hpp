31c29d8e1bcaa8239f06e3184dc22248f7847aad
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-03 06:38:22 +0000

    Doxygen

--- api/gluon/edit/SpinBox.hpp
+++ api/gluon/edit/SpinBox.hpp
@@ -13,6 +13,11 @@
 
 namespace yq::gluon {
 
+    //! \brief Spin box with undo
+    //!
+    //! Well, once implemeneted, this will be a spin box with  the undo
+    //! capability, we're using this in the meanwhile so it's a single 
+    //! point addition.
     class SpinBox : public QSpinBox, public UndoBase {
         Q_OBJECT
     public:
@@ -20,8 +25,12 @@ namespace yq::gluon {
         ~SpinBox();
         
         //  TODO: Currently, the *ACTUAL* undo tie in is not yet implemented
-        void                        set(const std::optional<unsigned int>&, unsigned int z=0);
-        std::optional<unsigned int> getUInt(unsigned int z=0) const;
+        
+        //! Set the value (along with a parameter in case it's undefined)
+        void                        set(const std::optional<unsigned int>& zValue, unsigned int zDefault=0);
+        
+        //! Gets the value
+        std::optional<unsigned int> getUInt(unsigned int zDefault=0) const;
 
     public slots:   
         virtual void    reset();
