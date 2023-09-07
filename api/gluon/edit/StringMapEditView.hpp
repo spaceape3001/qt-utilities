34ed7994c02c508b9ceb245b86a76fa913d82c91
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-07 09:41:16 +0000

    Doxygen

--- api/gluon/edit/StringMapEditView.hpp
+++ api/gluon/edit/StringMapEditView.hpp
@@ -11,13 +11,17 @@
 
 namespace yq::gluon {
 
-
+    //! View for the string map edit
     class StringMapEdit::View : public QTableView {
         Q_OBJECT
     public:
+        //! Constructor 
         View(Model*);
+        
+        //! Destructor
         ~View();
         
+        //! Returns the currently selected rows
         std::vector<int>	selectedRows() const;
     };
 }
