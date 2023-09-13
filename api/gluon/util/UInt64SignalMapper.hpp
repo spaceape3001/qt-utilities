1cef78e2565e5812bfd75641e1933abdafd7c1c3
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-13 10:44:33 +0000

    Doxygen

--- api/gluon/util/UInt64SignalMapper.hpp
+++ api/gluon/util/UInt64SignalMapper.hpp
@@ -10,19 +10,29 @@
 #include <map>
 
 namespace yq::gluon {
+    /*! \brief Signal Mapper that maps using an UNSIGNED uint64 integer
+    */
     class UInt64SignalMapper : public QObject {
         Q_OBJECT
     public:
+        //! Constructor
         UInt64SignalMapper(QObject* parent=nullptr);
+        
+        //! Destructor
         virtual ~UInt64SignalMapper ();
 
+        //! Sets the object's mapping
         void        setMapping(QObject*, uint64_t);
+        
+        //! Removes the object from the map
         void        removeMappings(QObject*);
 
     signals:
+        //! Emitted when a mapped signal is triggered
         void        mapped(uint64_t);
 
     public slots:
+        //! Used by the objects to connect their signals to this one
         void        map();
 
     private slots:
