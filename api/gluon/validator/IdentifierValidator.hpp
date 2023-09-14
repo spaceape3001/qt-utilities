32144de3a225d1fdfe704836d1b01b5e9a8c4e0c
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-14 09:02:33 +0000

    Doxygen

--- api/gluon/validator/IdentifierValidator.hpp
+++ api/gluon/validator/IdentifierValidator.hpp
@@ -9,13 +9,25 @@
 #include <QValidator>
 
 namespace yq::gluon {
+
+    /*! \brief Validator for Identifiers
+    
+        A valid identifier is one that starts with a letter, optionally followed
+        by one or more underscores, letters, and/or numbers
+    */
     class IdentifierValidator : public QValidator {
         Q_OBJECT
     public:
+        //! Constructor
         IdentifierValidator(QObject*parent=nullptr);
+        
+        //! Destructor
         ~IdentifierValidator();
         
+        //! Fixes the input (ie delete offenders)
         void                fixup(QString&input) const override;
+        
+        //! Validation method
         QValidator::State   validate(QString& input, int& pos) const override;
     };
 }
