c37093f1e5ba5aa8636375c6f05bd22f950cb6c1
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-15 05:45:37 +0000

    Doxygen

--- api/gluon/validator/NoSpacesValidator.hpp
+++ api/gluon/validator/NoSpacesValidator.hpp
@@ -9,13 +9,24 @@
 #include <QValidator>
 
 namespace yq::gluon {
+    /*! \brief Validator that disallows spaces
+    
+        Simple validator that rejects all whitespace
+    */
     class NoSpacesValidator : public QValidator {
         Q_OBJECT
     public:    
+    
+        //! Constructor
         NoSpacesValidator(QObject*parent=nullptr);
+        
+        //! Destructor
         ~NoSpacesValidator();
 
+        //! Fixes the string (ie deletes the spaces)
         void                fixup(QString&input) const override;
+        
+        //! Validates the string
         QValidator::State   validate(QString& input, int& pos) const override;
     };
 }
