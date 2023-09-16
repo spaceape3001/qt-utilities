1983af50ef9255526097362a922aff31505cf4b4
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-16 08:02:15 +0000

    Doxygen

--- api/gluon/validator/XmlIdentifierValidator.hpp
+++ api/gluon/validator/XmlIdentifierValidator.hpp
@@ -9,15 +9,27 @@
 #include <QValidator>
 
 namespace yq::gluon {
+    /*! \brief Validator for Xml tags
+    
+        This validates the input to make sure it matches valid HTML tags
+    */
     class XmlIdentifierValidator : public QValidator {
         Q_OBJECT
     public:
+    
+        //! Constructor
         XmlIdentifierValidator(QObject*parent=nullptr);
+        
+        //! Destructor
         ~XmlIdentifierValidator();
         
+        //! Static is valid checker
         static bool     isValid(const QString&, int* pos=nullptr);
         
+        //! Fixes up the input
         void                fixup(QString&input) const override;
+        
+        //! Validates the input
         QValidator::State   validate(QString& input, int& pos) const override;
     };
 }
