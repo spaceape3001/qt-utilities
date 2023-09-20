db879dfc0cd5b675fe45e46b68b16c1d2c8987bb
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-20 06:41:06 +0000

    Doxygen

--- api/gluon/widget/SimpleTable.hpp
+++ api/gluon/widget/SimpleTable.hpp
@@ -14,16 +14,27 @@
 class  SimpleTable : public QTableView {
     Q_OBJECT
 public:
+
+    //! Constructor
     SimpleTable(QWidget*parent=nullptr);
 
+    //! Sets the specified data
     bool    set(int,int,const QVariant&);
+    
+    //! Sets the header (also primes the array to the right column width)
     void    setHeader(const Vector<QString>&);
 
     class Model;
+    
+    //! Model
     Model*  model() { return m_model; }
+    
+    //! Model
     const Model* model() const { return m_model; }
 
 public slots:
+
+    //! Clears the data
     void        clear();
 //    void        fit();
 
@@ -31,22 +42,39 @@ private:
     Model*      m_model;
 };
 
+/*! \brief Simple Table Model
+*/
 class SimpleTable::Model : public QAbstractTableModel {
     Q_OBJECT
 public:
 
+    //! Constructor
     Model(QObject* parent=nullptr);
+    
+    //! Destructor
     ~Model();
 
+    //! Sets the header
     void        setHeader(const Vector<QString>&);
-    int         rowCount(const QModelIndex&) const { return m_data.rowCount(); }
-    int         columnCount(const QModelIndex&) const { return m_data.columnCount(); }
-    QVariant    data(const QModelIndex&, int) const;
-    QVariant    headerData(int, Qt::Orientation, int) const;
+    
+    //! Current row count
+    int         rowCount(const QModelIndex&) const override { return m_data.rowCount(); }
+    
+    //! Column count
+    int         columnCount(const QModelIndex&) const override { return m_data.columnCount(); }
+    
+    //! Data
+    QVariant    data(const QModelIndex&, int) const override;
+    
+    //! Gets header data
+    QVariant    headerData(int, Qt::Orientation, int) const override;
 
+    //! Sets data
     bool        set(int,int,const QVariant&);
 
 public slots:
+
+    //! Clears the data
     void        clear();
 
 private:
