09ebb9e4c65c42d7f778b6d20c0f1670bb947f0a
spaceape3001 <spaceape3001@users.noreply.github.com>
2023-09-21 06:21:38 +0000

    Doxygen

--- api/gluon/widget/GraphicsView.hpp
+++ api/gluon/widget/GraphicsView.hpp
@@ -15,24 +15,41 @@ namespace yq::gluon {
         Q_OBJECT
     public:
     
+        //! Steps to double zoom
         static constexpr const unsigned kDOUBLE = 8;    // eight steps to double
     
+        //! Constructor
         GraphicsView(QGraphicsScene*, QWidget*parent=nullptr);
+        
+        //! Destructor
         ~GraphicsView();
         
+        //! Zoom factor
         double      zoomFactor() const;
         
     public slots:
+        //! Adjusts the zoom to fit the scene
         void        fitToScene();
+        
+        //! Zooms in one step
         void        zoomIn();
+
+        //! Zooms out one step
         void        zoomOut();
+        
+        //! Sets the zoom factor
         void        setZoomFactor(double);
         
     signals:
+        //! Advertises the current mouse position in the view
         void    mouseAt(double, double);
     
     protected:
+    
+        //! Scroll wheel event handler (override)
         void    wheelEvent(QWheelEvent *event) override;
+        
+        //! Mouse move event handler (override)
         void    mouseMoveEvent(QMouseEvent*) override;
     };
 }
