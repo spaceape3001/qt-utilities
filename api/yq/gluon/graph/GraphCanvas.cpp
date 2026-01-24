d79a7eac05ca036bfd2527c31797009d56f26c82
spaceape3001 <spaceape3001@users.noreply.github.com>
2026-01-24 20:36:30 +0000

    Graph Canvas & Cleanup
    Moving the canvas into here, updating to latest graph, and zapping the
    old overhead

---
+++ api/yq/gluon/graph/GraphCanvas.cpp
@@ -0,0 +1,91 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "GraphCanvas.hpp"
+
+#include <yq/text/match.hpp>
+#include <yq/graph/GDocument.hpp>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <yq/gluon/graph/GraphScene.hpp>
+#include <yq/gluon/graph/GraphView.hpp>
+
+
+namespace yq::gluon {
+    static std::atomic<unsigned>    gCanvasNum{1};
+
+    GraphCanvas::GraphCanvas(QWidget*parent) : 
+        GraphicsCanvas(new GraphView(new GraphScene), parent), 
+        m_number(gCanvasNum++)
+    {
+        m_view      = (GraphView*) GraphicsCanvas::view();
+        m_scene     = (GraphScene*) GraphicsCanvas::scene();
+        m_scene -> setParent(this);
+        m_scene -> setSceneRect(QRectF(0,0,2048,2048));
+    }
+    
+    GraphCanvas::~GraphCanvas()
+    {
+    }
+
+    void        GraphCanvas::clear()
+    {
+        m_scene -> clear();
+        m_url   = {};
+    }
+
+    GDocumentPtr           GraphCanvas::get() const
+    {
+        return {};
+        //return m_scene -> get();
+    }
+    
+    void                    GraphCanvas::set(const GDocument& doc)
+    {
+        //m_scene -> set(doc);
+        //m_url       = doc.url();
+    }
+
+    std::filesystem::path   GraphCanvas::dirpath() const
+    {
+        return filepath().parent_path();
+    }
+    
+    QString                 GraphCanvas::dirname() const
+    {
+        return QString::fromStdString(dirpath().string());
+    }
+
+    std::filesystem::path GraphCanvas::filepath() const
+    {
+        if(!is_similar(m_url.scheme, "file"))
+            return {};
+        if(!m_url.fragment.empty())
+            return {};
+        if(!m_url.query.empty())
+            return {};
+        return m_url.path;
+    }
+
+    QString GraphCanvas::filename() const
+    {
+        return QString::fromStdString(filepath().string());
+    }
+
+    void    GraphCanvas::updateTitle()
+    {
+        std::string s = to_string(m_url);
+        if(s.empty()){
+            setWindowTitle(tr("Unnamed:%1").arg(m_number));
+        } else {
+            setWindowTitle(QString::fromStdString(s));
+        }
+    }
+}
+
+
+
+#include "moc_GraphCanvas.cpp"
---
+++ api/yq/gluon/graph/GraphCanvas.hpp
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  YOUR QUILL
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <yq/core/Ref.hpp>
+#include <yq/gluon/graphics/GraphicsCanvas.hpp>
+#include <yq/net/Url.hpp>
+#include <yq/typedef/g_document.hpp>
+#include <yq/typedef/g_meta_graph.hpp>
+#include <filesystem>
+
+namespace yq::gluon {
+    class GraphScene;
+    class GraphView;
+
+    class GraphCanvas : public GraphicsCanvas {
+        Q_OBJECT
+    public:
+    
+        GraphCanvas(QWidget*parent=nullptr);
+        ~GraphCanvas();
+        
+        GraphScene*             scene() { return m_scene; }
+        const GraphScene*       scene() const { return m_scene; }
+        
+        GraphView*              view() { return m_view; }
+        const GraphView*        view() const { return m_view; }
+        
+        std::filesystem::path   dirpath() const;
+        QString                 dirname() const;
+        
+        std::filesystem::path   filepath() const;
+        QString                 filename() const;
+        
+        GDocumentPtr            get() const;
+        void                    set(const GDocument&);
+        
+        const Url& url() const { return m_url; }
+        
+
+    public slots:
+        void        updateTitle();
+        void        clear();
+        
+    private:
+        const unsigned          m_number;
+        Url                     m_url;
+        GraphScene*             m_scene     = nullptr;
+        GraphView*              m_view      = nullptr;
+    };
+
+}
